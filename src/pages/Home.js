9c465c61e96899a587b3f67a2ea5e1704bbb613a
Jack Joseph <dunleavyjack@gmail.com>
2021-03-15 21:49:21 +0900

    Fixed buttons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import stravaButton from '../assets/stravaButton.png'
-import demoButton from '../assets/demoButton.png'
+import stravaButton4X from '../assets/stravaButton4X.png'
+import demoButton4X from '../assets/demoButton4X.png'
 import personLogo from '../assets/personLogo.png'
 
 const handleLogin = () => {
@@ -14,10 +14,10 @@ const Home = () => {
                 <img src={personLogo} alt="logo" className="person-logo text-center"></img>
                 <h2 className="bold-poppins-small">Your activities, summed up.</h2>
                 <div>
-                    <img src={stravaButton} onClick={handleLogin} alt="login" className="login-button mb-3"></img>
+                    <img src={stravaButton4X} onClick={handleLogin} alt="login" className="login-button mb-3"></img>
                 </div>
                 <div>
-                    <img src={demoButton} alt="demo login" className="login-button"></img>
+                    <img src={demoButton4X} alt="demo login" className="login-button"></img>
                 </div>
             </div>
         </div>
