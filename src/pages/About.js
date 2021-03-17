1fa5a9a7f2ce0ea8ae014d215b6c5e577c43aec4
dunleavyjack <dunleavyjack@gmail.com>
2021-03-18 01:31:42 +0900

    Updated about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
     return (
         <div>
-            <h1>About page about page about page about page</h1>
+            <h1>About page about page about page about page heyyyy</h1>
         </div>
     )
 }
