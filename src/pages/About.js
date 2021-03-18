27f1deb6899f77190d12e39aeae0cc8dc53424fe
dunleavyjack <dunleavyjack@gmail.com>
2021-03-18 17:28:38 +0900

    Testing vercel connection

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
     return (
         <div>
-            <h1>About page about page about page about page heyyyy</h1>
+            <h1>About page about page about page about page hi heyyyy</h1>
         </div>
     );
 };
