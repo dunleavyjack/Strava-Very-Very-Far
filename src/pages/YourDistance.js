2a5455a296751c482a71751586852587c67b7f0f
dunleavyjack <dunleavyjack@gmail.com>
2021-03-25 11:10:33 +0900

    Reduced clutter

--- src/pages/YourDistance.js
+++ src/pages/YourDistance.js
@@ -1,23 +1,7 @@
 import React from "react";
-import { connect } from "react-redux";
-import {
-    changeUnits,
-    addSport,
-    removeSport,
-    setTotalDistance,
-} from "../actions";
 import BlockList from "../components/BlockList";
 
-const YourDistance = ({
-    userActivities: { runTotal, rideTotal, swimTotal },
-    userProfile,
-    changeUnits,
-    addSport,
-    sports,
-    metric,
-    removeSport,
-    setTotalDistance,
-}) => {
+const YourDistance = () => {
     return (
         <div className="container p-0">
             <BlockList />
@@ -25,18 +9,5 @@ const YourDistance = ({
     );
 };
 
-const mapStateToProps = (state) => {
-    return {
-        userActivities: state.userActivities,
-        userProfile: state.userProfile,
-        metric: state.metric,
-        sports: state.sports,
-    };
-};
 
-export default connect(mapStateToProps, {
-    changeUnits,
-    addSport,
-    removeSport,
-    setTotalDistance,
-})(YourDistance);
+export default YourDistance;
